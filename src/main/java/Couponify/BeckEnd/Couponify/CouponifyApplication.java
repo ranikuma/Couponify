66fde7fa559941644990068e7508edc5bfdc895f
ranikuma <ranitk.28@gmail.com>
2022-03-06 15:02:11 +0530

    Initial Commit

---
+++ src/main/java/Couponify/BeckEnd/Couponify/CouponifyApplication.java
@@ -0,0 +1,13 @@
+package Couponify.BeckEnd.Couponify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouponifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Couponify/BeckEnd/Couponify/CouponifyApplicationTests.java
@@ -0,0 +1,13 @@
+package Couponify.BeckEnd.Couponify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
